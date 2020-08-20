3fc1741b7455db362dd6a4286d5ceac315786772
karen <451390409@qq.com>
2020-08-21 06:47:28 +0800

    分支学习

---
+++ cha5/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var age int
+	fmt.Println("请输入你的年龄:")
+	fmt.Scanln(&age)
+	if age > 18 {
+		fmt.Println("你要对自己的行为负责了")
+	}else{
+		fmt.Println("你还小，放过你了")
+	}
+
+}
